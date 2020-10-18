9abc3405f3067ae00e0cdb526676143001acc7ea
Christopher Hunt <cbh808@gmail.com>
2020-10-18 23:31:37 +0200

    prep

--- RB101_small/medium2/med2q10_sum_square.rb
+++ RB101_small/medium2/med2q10_sum_square.rb
@@ -1,19 +1,34 @@
 =begin
+write methods that computes difference between
+ -square of the sum of the first n positive integers
+ -sum of the squares of the first n positive integers.
 # inputs:
-
+integer n as arg
 # outputs:
-
+integer of result
 # rules:
 
 # Example / Test Case:
 see code
 
 # Data Structures:
-input
-output
+input integer (n)
+output integer
 
 # Algorithm:
+define method sum_square_difference with one arg (int)
+calculate all integers from 1 to n and carry out the calculations
+ (1 + ... + n)**2  - (1**2 + ... + n**2)
+e.g. use range (1..n).inject(:+) - (1..n).inject(0) {|sum, x| sum += x**2}
 
 =end 
+def sum_square_difference(n)
+  (1..n).inject(:+)**2 - (1..n).inject(0) {|sum, x| sum += x**2}
+end
 
 
+p sum_square_difference(3) == 22
+   # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+p sum_square_difference(10) == 2640
+p sum_square_difference(1) == 0
+p sum_square_difference(100) == 25164150
