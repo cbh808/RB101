39e7bfee68686e3aa9806adde2f9733864f43721
cbh808 <cbh808@gmail.com>
2021-07-10 17:58:09 +0200

    file names

---
+++ RB101_small/easy2/e2q9_string_assignment.rb
@@ -0,0 +1,7 @@
+=begin
+in second example, the object being pointed to by
+name and save_name is the same and pointer does not change
+when the object is mutated by upcase!, therefore
+puts name, save_name will yield
+BOB
+BOB
